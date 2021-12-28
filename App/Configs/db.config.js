59d1d9fa3f6cff792b9beebe6ca46f15edccad52
BangBang <phibang7899@gmail.com>
2021-12-28 23:34:49 +0700

    fix sequelize

--- App/Configs/db.config.js
+++ App/Configs/db.config.js
@@ -1,9 +1,19 @@
+require('dotenv').config()
+
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "01232525113",
-  DB: "jira_modal",
-  dialect: "mysql",
+  HOST: process.env.host,
+  USER: process.env.username,
+  PASSWORD: process.env.password,
+  DB: process.env.database,
+  dialect: process.env.dialect,
+
+
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "01232525113",
+  // DB: "jira_modal",
+  // dialect: "mysql",
   
 }
 
